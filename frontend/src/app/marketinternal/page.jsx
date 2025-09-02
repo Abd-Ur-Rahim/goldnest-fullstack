c2686a710493513ae83cdc8e76a6daa8058e0785
Rahim Iqbal <mirahim2003@gmail.com>
2025-09-02 06:56:45 +0530

    Update page.jsx

--- frontend/src/app/marketinternal/page.jsx
+++ frontend/src/app/marketinternal/page.jsx
@@ -66,7 +66,7 @@ export default function MarketInternalPage() {
         // Set up interval for periodic fetching
         const intervalId = setInterval(() => {
             fetchData();
-        }, 7200000); // 30 seconds
+        }, 600000); // 30 seconds
 
         // Cleanup on unmount
         return () => clearInterval(intervalId);
@@ -252,4 +252,5 @@ export default function MarketInternalPage() {
     );
 
 }
+
 
